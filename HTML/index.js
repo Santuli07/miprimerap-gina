0cbe3eb84dcbd0f2dc8aa29001a2d12d1108d9c0
Santuli07 <135279552+Santuli07@users.noreply.github.com>
2023-06-14 19:05:25 -0300

    Initial commit

---
+++ HTML/index.js
@@ -0,0 +1,6 @@
+// Enucnetra al elemento HTML (el botón) mediante su ID
+const miBotón = document.getElementById('miBoton');
+// Define la función que se ejecutará cuando se haga clic en el botón 
+function handleClick() { 
+    alert('¡Hola! Has hecho clic en el botón.');
+}
